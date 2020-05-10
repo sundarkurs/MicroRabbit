0cd535e7dc6ceefbed120d52e18eed95b9c39a9f
Sundar Urs <sundarkurs@gmail.com>
2020-05-10 17:49:02 +0530

    Added Transfer Repository and Service implementation

---
+++ MicroRabbit/MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -0,0 +1,38 @@
+﻿
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Application.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly ITransferRepository _transferRepository;
+        private readonly IEventBus _bus;
+
+        public TransferService(ITransferRepository transferRepository, IEventBus bus)
+        {
+            _transferRepository = transferRepository;
+            _bus = bus;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _transferRepository.GetTransferLogs();
+        }
+
+        //public void Transfer(AccountTransfer accountTransfer)
+        //{
+        //    var createTransferCommand = new CreateTransferCommand(
+        //            accountTransfer.FromAccount,
+        //            accountTransfer.ToAccount,
+        //            accountTransfer.TransferAmount
+        //        );
+
+        //    _bus.SendCommand(createTransferCommand);
+        //}
+    }
+}
