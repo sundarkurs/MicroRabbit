ee18e6938848219a76d77654061b62a77a6e2d6f
Sundar Urs <sundarkurs@gmail.com>
2020-05-10 12:47:22 +0530

    Implemented Transfer and RabbitMQ publish flow

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -3,6 +3,8 @@ using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -15,11 +17,16 @@ namespace MicroRabbit.Infra.IoC
         public static void RegisterServices(IServiceCollection services)
         {
             //Domain Bus
-            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
-            {
-                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
-                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
-            });
+            services.AddSingleton<IEventBus, RabbitMQBus>();
+
+            //services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            //{
+            //    var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+            //    return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            //});
+
+            // Domain banking commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             // Application Services
             services.AddTransient<IAccountService, AccountService>();
